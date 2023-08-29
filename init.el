422a3928adf168a99829df746e53c06103e13474
Chris Jobling <cpjobling@cpjobling.net>
2023-08-29 09:50:39 +0100

    Following along EFS as far as 22.09

---
+++ init.el
@@ -0,0 +1,52 @@
+;; ~/.emacs.default/init.el
+;; Based on System Crafters
+;; ![Emacs from Scratch](https://www.youtube.com/live/74zOY-vgkyw?si=_2SNVezXLoDQ4WZC)
+
+(setq inhibit-startup-message t)
+
+(scroll-bar-mode -1)        ; Disable visible scrollbar
+(tool-bar-mode -1)          ; Disable the toolbar
+(tooltip-mode -1)           ; Disable tooltips
+(set-fringe-mode 10)        ; Give some breathing room
+
+(menu-bar-mode -1)            ; Disable the menu bar
+
+;; Set up the visible bell
+(setq visible-bell t)
+
+(set-face-attribute 'default nil :font "Fira Code Retina" :height 280)
+
+(load-theme 'tango-dark)
+
+;; Initilize package sources
+(require 'package)
+
+(setq package-archives '(("melpa" . "https://melpa.org/packages/")
+                         ("org" . "https://orgmode.org/elpa/")
+                         ("elpa" . "https://elpa.gnu.org/packages/")))
+
+(package-initialize)
+(unless package-archive-contents
+ (package-refresh-contents))
+
+;; Initialize use-package on non-Linux platforms
+(unless (package-installed-p 'use-package)
+   (package-install 'use-package))
+
+(require 'use-package)
+(setq use-package-always-ensure t)
+
+(use-package command-log-mode)
+
+
+
+
+
+
+
+
+
+
+
+
+
