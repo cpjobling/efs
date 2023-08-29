829b124200d817069b4dcbb30fe01ee2c5a8fe0d
Chris Jobling <cpjobling@cpjobling.net>
2023-08-29 12:21:15 +0100

    End of tutorial 1

--- init.el
+++ init.el
@@ -18,6 +18,9 @@
 
 (load-theme 'tango-dark)
 
+;; Make ESC quit prompts
+(global-set-key (kbd "<escape>") 'keyboard-escape-quit)
+
 ;; Initilize package sources
 (require 'package)
 
@@ -37,6 +40,38 @@
 (setq use-package-always-ensure t)
 
 (use-package command-log-mode)
+(global-command-log-mode)
+(use-package ivy
+  :diminish
+  :bind (("C-s" . swiper)
+         :map ivy-minibuffer-map
+         ("TAB" . ivy-alt-done)	
+         ("C-l" . ivy-alt-done)
+         ("C-j" . ivy-next-line)
+         ("C-k" . ivy-previous-line)
+         :map ivy-switch-buffer-map
+         ("C-k" . ivy-previous-line)
+         ("C-l" . ivy-done)
+         ("C-d" . ivy-switch-buffer-kill)
+         :map ivy-reverse-i-search-map
+         ("C-k" . ivy-previous-line)
+         ("C-d" . ivy-reverse-i-search-kill))
+  :config
+  (ivy-mode 1))
+
+(use-package counsel
+  :bind (("M-x" . counsel-M-x)
+	 ("C-x b" . counsel-ibuffer)
+	 ("C-x C-f" . counsel-find-file)
+	 ;;:map mini-buffer-local-map ;; doesn't work in this version of init.el
+	 ("C-r" . "counsel-minibuffer-history"))
+  :config
+  (setq ivy-initial-inputs-alist nil)) ;; Don't start searches with ^
+	 
+(use-package doom-modeline
+  :ensure t
+  :init (doom-modeline-mode 1)
+  :custom ((doom-modeline-height 15)))
 
 
 
@@ -46,7 +81,15 @@
 
 
 
-
-
-
-
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(package-selected-packages '(doom-modeline counsel ivy command-log-mode)))
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ )
